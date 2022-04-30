04cfce9a336d80923a454be230269177c781f9d2
George Cheng <sparklyspuds@gmail.com>
2022-04-30 16:31:41 +0800

    init & webpack done

---
+++ client/src/js/database.js
@@ -0,0 +1,65 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async () => {
+  try {
+    console.log("Updating Process");
+    // create connection to db and pick the version
+    const jateDb = await openDB("jate", 1);
+    // create a new transaction with the handle type
+    const tx = jateDb.transaction("jate", "readwrite");
+    
+    const store = tx.objectStore("jate");
+    
+    const req = store.get({ id:1,data:content });
+    
+    const result = await req;
+
+    console.log("All Data", result);
+
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+
+};
+
+console.error("putDb not implemented");
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    console.log("Retriving all content");
+    // create connection to db and pick the version
+    const jateDb = await openDB("jate", 1);
+    // create a new transaction with the handle type
+    const tx = jateDb.transaction("jate", "readonly");
+
+    const store = tx.objectStore("jate");
+    const id = 1;
+    const req = store.get({ id });
+    const result = await req;
+
+    console.log("All Data", result);
+
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+console.error("getDb not implemented");
+
+initdb();
